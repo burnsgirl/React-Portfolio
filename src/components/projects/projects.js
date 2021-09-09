8a80eaf38ce0eae4df237ecf1a095267c48ac940
Dauphine Burns <dauphineburns@gmail.com>
2021-09-09 14:25:36 -0400

    fixed onclick linkin projects

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './projects.css';
 
-function Project () {
+function Project (props) {
     return (
         <div class="container">
 
             <div class="card">
                 <div class="overview" >
-                    <p></p>
+                    <p>{props.overview}</p>
                 </div>
                 <div class="mockup">
-                    <h1></h1>
-                    <img />
+                    <h1>{props.mockup}</h1>
+                    <img>{props.image}</img>
                     <div class="links">
-                        <button class="btn-deployed" onclick="window.location.">Deployed</button>
-                        <button class="btn-github" onclick="window.location.">GitHub</button>
+                        <button class="btn-deployed" onclick="window.location.{props.btn-deployed}">Deployed</button>
+                        <button class="btn-github" onclick="window.location.{props.btn-github}">GitHub</button>
                     </div>
                 </div>  
             </div>
