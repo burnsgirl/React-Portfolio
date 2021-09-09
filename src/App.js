49f4cb2541c4abe32a45349f864b46894dbfcf58
Dauphine Burns <dauphineburns@gmail.com>
2021-09-09 14:04:28 -0400

    project array outline added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Doctype from './components/doctype';
 import Header from './components/header/header';
 import Project from './components/projects/projects';
+import projects from './project.json';
 
 function App() {
   return (
