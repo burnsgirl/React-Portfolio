f132a216156df12aba9e195ecb669cd95a11d753
Dauphine Burns <dauphineburns@gmail.com>
2021-09-09 14:16:46 -0400

    project outline added

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -3,7 +3,22 @@ import './projects.css';
 
 function Project () {
     return (
+        <div class="container">
 
+            <div class="card">
+                <div class="overview" >
+                    <p></p>
+                </div>
+                <div class="mockup">
+                    <h1></h1>
+                    <img />
+                    <div class="links">
+                        <button class="btn-deployed" onclick="window.location.">Deployed</button>
+                        <button class="btn-github" onclick="window.location.">GitHub</button>
+                    </div>
+                </div>  
+            </div>
+        </div>
     )
 }
 
