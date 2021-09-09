001779c379c3cf66f00106d576cfee5c106933d3
Dauphine Burns <dauphineburns@gmail.com>
2021-09-09 13:21:58 -0400

    added head function

---
+++ src/components/doctype.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+function Head() {
+
+    return (
+
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8"></meta>
+            <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+            <title>Dauphine Burns Portfolio</title>
+            <link rel="stylesheet" href="styles/home.css"/>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap" rel="stylesheet"/>
+        
+        
+        </head>
+        
+        </html>
+    )
+
+}
+
+export default Head;
