bc6c2061dab789228be431a0ac58448f65e28e1c
Dauphine Burns <dauphineburns@gmail.com>
2021-09-09 14:35:19 -0400

    added outline for each project

--- src/App.js
+++ src/App.js
@@ -18,11 +18,37 @@ function App() {
       btn-deployed={project[0].btn-deployed}
       btn-github={project[0].btn-github}
       />
-      <Projects />
-      <Projects />
-      <Projects />
-      <Projects />
-      <Projects />
+      <Projects 
+      mockup={project[1].mockup}
+      image={project[1].image}
+      overview={project[1].overview}
+      btn-deployed={project[1].btn-deployed}
+      btn-github={project[1].btn-github}
+      />
+      <Projects mockup={project[2].mockup}
+      image={project[2].image}
+      overview={project[2].overview}
+      btn-deployed={project[2].btn-deployed}
+      btn-github={project[2].btn-github}
+      />
+      <Projects mockup={project[3].mockup}
+      image={project[3].image}
+      overview={project[3].overview}
+      btn-deployed={project[3].btn-deployed}
+      btn-github={project[3].btn-github}
+      />
+      <Projects mockup={project[4].mockup}
+      image={project[4].image}
+      overview={project[4].overview}
+      btn-deployed={project[4].btn-deployed}
+      btn-github={project[4].btn-github}
+      />
+      <Projects 
+      mockup={project[5].mockup}
+      image={project[5].image}
+      overview={project[5].overview}
+      btn-deployed={project[5].btn-deployed}
+      btn-github={project[5].btn-github}/>
     </>
 
 
