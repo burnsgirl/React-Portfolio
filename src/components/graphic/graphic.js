8cddd3195dae6bd128e66b2f6452bb6ea452c741
Dauphine Burns <dauphineburns@gmail.com>
2021-09-10 20:51:34 -0400

    added graphic files

---
+++ src/components/graphic/graphic.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './graphic.css';
+import '../../../src/graphic.json';
+
+
+function Graphic () {
+
+    return(
+        <div class="container" id="graphic">
+            <div class="graphic-card">
+                <h1>GOOGLE PLAY</h1>
+                        <img id="google" src="../public/Assets/Graphic Design/Google.png" alt="Graphic image of Google Play logo."/>
+            </div>
+        </div>
+    )
+
+}
+
+export default Graphic;
