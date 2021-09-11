73d306282ebf4f654f7f78ca3028a7d953288aa2
Dauphine Burns <dauphineburns@gmail.com>
2021-09-11 08:35:17 -0400

    added basic styling and content to footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,15 @@
+mport React from 'react';
+import "./footer.css";
+
+function Footer () {
+    <div class="footer">
+        <ul>
+            <a id="numberf" href="#about">210-722-1739 |</a>
+            <a id="emailf" href="#about">dauphineburns@gmail.com |</a>
+            <a href="https://www.linkedin.com/in/dauphine-burns-b2b289134/" id="linkedinf">LinkedIn |</a>
+            <a href="https://github.com/burnsgirl"  id="githubf">GitHub</a>
+        </ul>
+    </div>
+}
+
+export default Footer;
