2f92b7649a9427aa2a8cbd84b04226b0f49cb1b2
Dauphine Burns <dauphineburns@gmail.com>
2021-09-10 20:46:50 -0400

    removed unecessary doctype file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import './App.css';
-import Doctype from './components/doctype';
+
 import Header from './components/header/header';
 import Projects from './components/projects/projects';
 import project from './project.json';
@@ -14,7 +14,6 @@ class App extends Component {
   render() {
     return (
       <>
-      <Doctype />
       <Header />
       {this.state.project.map(projects => (
         <Projects 
